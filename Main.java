b00ea6de5394e20ab43d66a7db4c9ffb37437bee
Lucas Rodrigues <lucas.fogaca@unilasalle.edu.br>
2019-11-19 22:46:39 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+class Main {
+  public static void main(String[] args) {
+     calculadora calc= new calculadora();
+     System.out.println(calc.calcula(1,1));
+    System.out.println(calc.calcula(2.0,6.1));
+    System.out.println(calc.calcula("vi","ram?"));
+  }
+}
---
+++ calculadora.java
@@ -0,0 +1,12 @@
+public class calculadora{
+ public int calcula(int a,int b){
+       return a+b;
+ }
+  public double calcula(double a,double b){
+        return a+b;
+   }
+  public String calcula(String a,String b){
+        return a+b;
+  }
+  
+}
